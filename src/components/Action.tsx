453349a9288388065fbc66c1e43ace975c462241
Ion <ion@Ions-MacBook-Pro.local>
2024-12-22 17:44:41 +0200

    Button action instead of span

--- src/components/Action.tsx
+++ src/components/Action.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@radix-ui/themes";
+
 export default function Action() {
   return (
     <div className="rounded-full p-[1px] bg-gradient-custom cursor-pointer hover:bg-gradient-custom-invert">
-      <div className="rounded-full p-2 bg-background-tertiary large-text">
-        <span>Firm</span>
-      </div>
+      <Button className="rounded-full p-2 bg-background-tertiary large-text">
+        Firm
+      </Button>
     </div>
   );
 }
