ba01ca5c6bf19071c08bd94c549ea8cf6b4fae7c
Ion <ion@Ions-MacBook-Pro.local>
2024-12-22 17:49:55 +0200

    Action width

--- src/components/Action.tsx
+++ src/components/Action.tsx
@@ -3,7 +3,7 @@ import { Button } from "@radix-ui/themes";
 export default function Action() {
   return (
     <div className="rounded-full p-[1px] bg-gradient-custom cursor-pointer hover:bg-gradient-custom-invert">
-      <Button className="rounded-full p-2 bg-background-tertiary large-text">
+      <Button className="rounded-full p-2 bg-background-tertiary large-text w-full">
         Firm
       </Button>
     </div>
